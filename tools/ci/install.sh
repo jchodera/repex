b3efbb54b50417811d972303b9d6752a853989d6
John Chodera <choderaj@mskcc.org>
2014-07-01 21:06:42 -0700

    Added choderalab channel for pymbar distribution.

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -9,5 +9,9 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
+conda config --add channels http://conda.binstar.org/choderalab
 conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
 source activate $python
+
+# Need to install nose-exclude
+pip install nose-exclude
